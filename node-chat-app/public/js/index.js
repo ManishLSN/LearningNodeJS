8ae97c02dd3153e7b34bb7cb52bd472c55d77795
Ankit Dutta <geekankit318@gmail.com>
2017-03-21 11:35:04 +0530

    Emit newMessage on createMessage

--- node-chat-app/public/js/index.js
+++ node-chat-app/public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
     console.log('Connected to server.');
-
-    socket.emit('createMessage', {
-        from: 'Harpreet',
-        text: 'Hello how are you?'
-    });
 });
 
 socket.on('disconnect', function () {
